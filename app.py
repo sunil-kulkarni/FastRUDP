e704e42e86301f933090dd9bf39f0e7d712e3fbc
Sunil Kulkarni <157889773+sunil-kulkarni@users.noreply.github.com>
2026-04-22 21:23:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,3 @@
-# from fastapi import FastAPI
-# from fastapi.responses import HTMLResponse
-# from fastapi.staticfiles import StaticFiles
-# from fastapi.templating import Jinja2Templates
-# from starlette.requests import Request
-
-# from udp_client import request_file
-
-# app = FastAPI()
-
-# templates = Jinja2Templates(directory="templates")
-
-# app.mount("/static", StaticFiles(directory="static"), name="static")
-
-
-# @app.get("/", response_class=HTMLResponse)
-# def home(request: Request):
-#     return templates.TemplateResponse("index.html", {"request": request})
-
-
-# @app.get("/download/{filename}")
-# def download(filename: str):
-
-#     success = request_file(filename)
-
-#     if success:
-#         return {"status": "success"}
-#     else:
-#         return {"status": "failed"}
 from fastapi import FastAPI, Request
 from fastapi.responses import StreamingResponse
 from fastapi.staticfiles import StaticFiles
